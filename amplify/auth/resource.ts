615ba6ebda0bab96db87b3367227513caf3c4395
Satya Prakash Jha <satyapj@gmail.com>
2025-01-19 23:35:38 +0530

    connect to bedrock

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,12 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailStyle: 'CODE',
+      verificationEmailSubject: 'Welcome to the AI-Powered SPJ Split-It App',
+      verificationEmailBody(createCode) {
+        return `Use this code to confirm your account: ${createCode()} `
+      },
+    },
   },
 });
--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,34 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+
+const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
+  "bedrockDS",
+  "https://bedrock-runtime.ap-south-1.amazonaws.com",
+  {
+    authorizationConfig: {
+      signingRegion: "ap-south-1",
+      signingServiceName: "bedrock",
+    },
+  }
+);
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
+  new PolicyStatement({
+    resources: [
+      // "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0",
+      "arn:aws:bedrock:ap-south-1::foundation-model/amazon.titan-text-express-v1",
+    ],
+    actions: ["bedrock:InvokeModel"],    
+  })
+);
